290d98b69a4b8cfb0b4a9f00053e164916b62dc4
Alex Bozarth <ajbozart@us.ibm.com>
2020-03-03 12:31:26 -0800

    Export the generator functions
    Allowing for thier use in adding additional trackers for similar
    file types in other extensions
    
    Fixes #119

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@
 // Distributed under the terms of the Modified BSD License.
 
 // Note: keep in alphabetical order...
+export * from './generators';
 export * from './registry';
 export * from './toc';
